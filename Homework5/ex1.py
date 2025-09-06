a1d45fc657a45bbeada6477c05ccde958df9a8fa
Fuad Murad <murad.fuad24@gmail.com>
2025-09-06 10:50:56 -0300

    Finish Homework 5

--- Homework5/ex1.py
+++ Homework5/ex1.py
@@ -9,6 +9,30 @@ seriesAno2 = pd.Series({"C" : 16.25, "Python" : 16.04, "Java" : 9.85})
 
 #2) 
 
-porcent = seriesAno1 + seriesAno2
+print("Mostrando a soma das linguagens nos dois anos: ")
 
-print(porcent)
+print(seriesAno1.add(seriesAno2, fill_value=0))
+
+print("\n")
+#3) 
+
+print("Apresentando o aumento e declinio das linguagens: " )
+
+popularidade =  seriesAno2.sub(seriesAno1, fill_value=0)
+
+print(popularidade)
+
+print("\n")
+
+#4)
+print("Mostrando as linguagens que teve um crescimento: ")
+
+print(popularidade[popularidade >= 0])
+
+print("\n")
+#5)
+
+two_years = popularidade * 2 # linguagem popular daqui a 2 anos 
+
+print("Mostrando a linguagem que mais cresceu em 2 anos: ") 
+print(two_years.nlargest(1))
