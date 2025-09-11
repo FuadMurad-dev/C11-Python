6d840af93f45a4c7a05d662524ca8f56e81b909e
Fuad Murad <murad.fuad24@gmail.com>
2025-09-10 23:37:54 -0300

    arrumando a ordem dos prints

--- Ex_class/ex1.py
+++ Ex_class/ex1.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-dataset = np.loadtxt("C:/Users/murad/OneDrive/Documentos/C11-Python/Ex_class/paises.csv", delimiter = ";" , dtype= str, encoding = "utf-8")
+dataset = np.loadtxt("C:/Users/murad/OneDrive/Área de Trabalho/C11-Python/Ex_class/paises.csv", delimiter=";", dtype=str, encoding="utf-8")
+
 
 coutry = dataset[0, : 4]
 
@@ -8,25 +9,18 @@ taxa_alfabetizacao = dataset[1:, 9].astype(float)
 
 taxa_media = taxa_alfabetizacao.mean()
 
-
 pais = dataset[:, 1]
 
 quantidade_america = np.sum(np.char.find(pais, "NORTHERN AMERICA" )!= -1)
-
 
 regiao = dataset[0: ,1]
 
 unica_regiao = np.unique(regiao)
 
-
-
-
-
-
 regiao_latam = np.where(regiao == "LATIN AMER. & CARIB    ")
 
 rendas = dataset[0: ,9]
-rendas_america = rendas[regiao_latam]
+rendas_america = rendas[regiao_latam] 
 local = dataset[0:,0]
 pais  = local[regiao_latam]
 maior_reda_caribe = np.argmax(rendas_america)
